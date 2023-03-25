a9238d929731bdf5437b661ca003c78484f7df52
Coding Raccon <egamyakub357@gmail.com>
2023-03-25 20:59:23 +0500

    The project is structured ad dashboard, hero, and main sections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashboardComponent } from './dashboard/dashboard';
+import { HeroComponent } from './hero/hero';
+import { MainComponent } from './main/main';
 
 @NgModule({
   declarations: [
     AppComponent, 
-    DashboardComponent
+    DashboardComponent, 
+    HeroComponent, 
+    MainComponent
   ],
   imports: [
     BrowserModule, 
