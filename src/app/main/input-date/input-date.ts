5340b9093a1f48f6e1c68f0fe9bd6bccc0234b12
Coding Raccon <egamyakub357@gmail.com>
2023-04-09 23:45:47 +0500

    app-main component restructured to one component, and css break-up fixed

--- src/app/main/input-date/input-date.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-input-date',
-  templateUrl: './input-date.html',
-  styleUrls: ['./input-date.css'],
-})
-export class InputDateComponent {}
--- src/app/main/input-note/input-note.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-input-note',
-  templateUrl: './input-note.html',
-  styleUrls: ['./input-note.css'],
-})
-export class InputNoteComponent {}
